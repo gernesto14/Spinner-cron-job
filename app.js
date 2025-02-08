8a50ae7d64d6ec012f11317e80172d0a57120ba7
gernesto14 <gernesto14@yahoo.com>
2025-02-07 22:54:46 -0500

    Cron job for keeping servers alive, simple.

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ import "dotenv/config";
 
 import express from "express";
 import path from "path";
-import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index.js";
-import usersRouter from "./routes/users.js";
+import axios from "axios";
 
 const app = express();
 
@@ -14,20 +13,52 @@ app.set(
   "views",
   path.join(path.dirname(new URL(import.meta.url).pathname), "views")
 );
+
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(
   express.static(
     path.join(path.dirname(new URL(import.meta.url).pathname), "public")
   )
 );
 
+app.use((req, res, next) => {
+  res.status(403).send("Forbidden");
+});
+
+// Function to send requests
+async function sendRequests() {
+  const urlsList = process.env.LIST_OF_URL;
+  const urls = urlsList.split(",");
+  try {
+    // Loop through the list of URLs and send a request to each one
+    urls.map(async (url) => {
+      console.log(`Sending request to ${url}`);
+      const response = await axios.get(url);
+
+      if (response.status === 200) {
+        console.log(`Request to ${url} was successful`);
+      } else {
+        console.error(
+          `Request to ${url} failed with status ${response.status}`
+        );
+      }
+    });
+    console.log("Requests sent successfully");
+  } catch (error) {
+    console.error("Error sending requests:", error);
+  }
+}
+
+// Set interval to call sendRequests
+const interval = process.env.INTERVAL_TIME;
+setInterval(sendRequests, interval);
+
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
